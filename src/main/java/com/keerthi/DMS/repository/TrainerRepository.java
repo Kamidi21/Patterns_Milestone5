7f05356f6c2c683682145e8a3d694d91a82a9a1f
Kamidi21 <kamidikeerthi21@gmail.com>
2024-11-23 22:06:59 -0600

    initial commit

---
+++ src/main/java/com/keerthi/DMS/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.keerthi.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.keerthi.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+
+}
